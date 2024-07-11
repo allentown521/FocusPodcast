5e2060b1265a427167d4f91a980d59edee68efed
allentown <69662387+allentown521@users.noreply.github.com>
2024-07-11 11:06:42 +0800

    Remove wearable support library(we only use 3 string constants in that whole library)

--- core/src/fdroid/java/allen/town/podcast/core/service/playback/WearMediaSession.java
+++ core/src/fdroid/java/allen/town/podcast/core/service/playback/WearMediaSession.java
@@ -3,7 +3,6 @@ package allen.town.podcast.core.service.playback;
 import android.os.Bundle;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
-import android.support.wearable.media.MediaControlConstants;
 
 public class WearMediaSession {
     static void sessionStateAddActionForWear(PlaybackStateCompat.Builder sessionState, String actionName,
@@ -11,7 +10,7 @@ public class WearMediaSession {
         PlaybackStateCompat.CustomAction.Builder actionBuilder =
                 new PlaybackStateCompat.CustomAction.Builder(actionName, name, icon);
         Bundle actionExtras = new Bundle();
-        actionExtras.putBoolean(MediaControlConstants.EXTRA_CUSTOM_ACTION_SHOW_ON_WEAR, true);
+        actionExtras.putBoolean("android.support.wearable.media.extra.CUSTOM_ACTION_SHOW_ON_WEAR", true);
         actionBuilder.setExtras(actionExtras);
 
         sessionState.addCustomAction(actionBuilder.build());
@@ -19,8 +18,8 @@ public class WearMediaSession {
 
     static void mediaSessionSetExtraForWear(MediaSessionCompat mediaSession) {
         Bundle sessionExtras = new Bundle();
-        sessionExtras.putBoolean(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_PREVIOUS, true);
-        sessionExtras.putBoolean(MediaControlConstants.EXTRA_RESERVE_SLOT_SKIP_TO_NEXT, true);
+        sessionExtras.putBoolean("android.support.wearable.media.extra.RESERVE_SLOT_SKIP_TO_PREVIOUS", true);
+        sessionExtras.putBoolean("android.support.wearable.media.extra.RESERVE_SLOT_SKIP_TO_NEXT", true);
         mediaSession.setExtras(sessionExtras);
     }
 }
